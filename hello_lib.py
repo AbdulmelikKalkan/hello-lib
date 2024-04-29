b50769e6c08300db1955fc9ac037b3ad4773c5ba
abdulmelikkalkan <kalkanabdulmelik@gmail.com>
2024-04-29 09:01:41 +0300

    first commit

---
+++ hello_lib.py
@@ -0,0 +1,3 @@
+def say_hello():
+  """Prints a friendly hello message."""
+  print("Hello from the hello_lib library!")
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='hello_lib',
+  version='1.0.0',
+  packages=find_packages(),
+  description='A simple Python library to print a hello message',
+)
